03512e0a158edba57e0fe3a7ae548e2714b30912
Luka Hartwig <luka.hartwig@vercel.com>
2024-01-03 21:12:05 +0100

    all in one component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,77 @@
-import { VercelPostgres } from "./vercel-postgres";
-import { VercelKv } from "./vercel-kv";
-import { Ioredis } from "./ioredis";
+import { sql } from "@vercel/postgres";
+import { kv } from "@vercel/kv";
+import { Redis } from "ioredis";
+import { createData } from "@/lib/data";
+import { unstable_noStore } from "next/cache";
+
+export const dynamic = "force-dynamic";
+
+const redis = new Redis(process.env.KV_URL_TLS!);
+
+async function VercelPostgres() {
+  const data = createData();
+  const t0 = performance.now();
+  await sql`INSERT INTO packages VALUES (${data.name}, ${data.version}, ${data.publishSize}, ${data.installSize}, ${data.publishFiles}, ${data.installFiles})`;
+  const t1 = performance.now();
+  const delta = t1 - t0;
+
+  return (
+    <div>
+      <h2>Vercel Postgres</h2>
+      <pre>
+        <code>INSERT INTO packages VALUES (?, ?, ?, ?, ?, ?)</code>
+      </pre>
+      <p>Took {delta}ms</p>
+    </div>
+  );
+}
+
+async function VercelKv() {
+  const data = createData();
+  const t0 = performance.now();
+  const { name, version, ...payload } = data;
+  const value = JSON.stringify(payload);
+  await kv.hset(name, { version, value });
+  const t1 = performance.now();
+  const delta = t1 - t0;
+
+  return (
+    <div>
+      <h2>Vercel KV</h2>
+      <pre>
+        <code>
+          HSET {name} version {version} value {value}
+        </code>
+      </pre>
+      <p>Took {delta}ms</p>
+    </div>
+  );
+}
+
+async function Ioredis() {
+  const data = createData();
+  const t0 = performance.now();
+  const { name, version, ...payload } = data;
+  const value = JSON.stringify(payload);
+  await redis.hset(name, version, value);
+  const t1 = performance.now();
+  const delta = t1 - t0;
+
+  return (
+    <div>
+      <h2>ioredis</h2>
+      <pre>
+        <code>
+          HSET {name} version {version} value {value}
+        </code>
+      </pre>
+      <p>Took {delta}ms</p>
+    </div>
+  );
+}
 
 export default function Home() {
+  unstable_noStore();
   return (
     <div style={{ display: "flex", flexDirection: "column", gap: "16px" }}>
       <VercelPostgres />
