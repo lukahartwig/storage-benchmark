384e0b47ed7c7364eae3fca86f79a69cf19825ba
Luka Hartwig <luka.hartwig@vercel.com>
2024-01-05 15:48:32 +0100

    add postgres client

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@ import { sql } from "@vercel/postgres";
 import { neonConfig } from "@neondatabase/serverless";
 import { kv } from "@vercel/kv";
 import { Redis } from "ioredis";
+import postgres from "postgres";
 import { createData } from "@/lib/data";
 import { unstable_noStore } from "next/cache";
 
 export const dynamic = "force-dynamic";
 
+const pg = postgres(process.env.POSTGRES_URL! + "?sslmode=require");
 const redis = new Redis(process.env.KV_URL_TLS!, {
   lazyConnect: true,
 });
@@ -16,13 +18,39 @@ neonConfig.fetchConnectionCache = true;
 async function VercelPostgres() {
   const data = createData();
   const t0 = performance.now();
-  await sql`INSERT INTO packages VALUES (${data.name}, ${data.version}, ${data.publishSize}, ${data.installSize}, ${data.publishFiles}, ${data.installFiles})`;
+  await sql`INSERT INTO packages VALUES (${data.name + "-vp"}, ${
+    data.version
+  }, ${data.publishSize}, ${data.installSize}, ${data.publishFiles}, ${
+    data.installFiles
+  })`;
   const t1 = performance.now();
   const delta = t1 - t0;
 
   return (
     <div>
       <h2>Vercel Postgres</h2>
+      <pre>
+        <code>INSERT INTO packages VALUES (?, ?, ?, ?, ?, ?)</code>
+      </pre>
+      <p>Took {delta}ms</p>
+    </div>
+  );
+}
+
+async function Postgres() {
+  const data = createData();
+  const t0 = performance.now();
+  await pg`INSERT INTO packages VALUES (${data.name + "-pg"}, ${
+    data.version
+  }, ${data.publishSize}, ${data.installSize}, ${data.publishFiles}, ${
+    data.installFiles
+  })`;
+  const t1 = performance.now();
+  const delta = t1 - t0;
+
+  return (
+    <div>
+      <h2>postgres</h2>
       <pre>
         <code>INSERT INTO packages VALUES (?, ?, ?, ?, ?, ?)</code>
       </pre>
@@ -80,6 +108,7 @@ export default function Home() {
   return (
     <div style={{ display: "flex", flexDirection: "column", gap: "16px" }}>
       <VercelPostgres />
+      <Postgres />
       <VercelKv />
       <Ioredis />
     </div>
